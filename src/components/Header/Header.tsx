0b509de24b960cc88adae5e5e728ae04951880b7
Hiago Gabriel <hiago@gmail.com>
2024-01-29 20:36:25 -0300

    implementando scss

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,23 @@
+import { StatsCard } from "../StactsCard/StatsCard";
+import styles from "./styles.module.scss";
+
 export const Header: React.FC = () => {
-  return <h1>Header</h1>
-} 
-  
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <div>
+          <h1>My Todo</h1>
+
+          <span>Bem-Vindo, Hiago</span>
+        </div>
+
+        <div>
+          <StatsCard title="Total de Tarefas" value={5}/>
+          <StatsCard title="Tarefas Pendentes" value={4} />
+          <StatsCard title="Tarefas Concluídas" value={1}/>
+        </div>
+
+      </div>
+    </header>
+  );
+};
